faf7df4f314cea56b6158fcfe2d8516038bc9d36
nicolaisss0999 <nd22070507nicoledurazo@gmail.com>
2025-11-21 14:52:48 -0700

    Fix comments and formatting in triangulo_simetrico

--- solucion.py
+++ solucion.py
@@ -1,15 +1,15 @@
 # En este archivo debes implementar la función
 
 def triangulo_simetrico(m: int, s: str) -> str:
-    # TODO: validar altura mayor que 0 e imprimir "Error: la altura debe ser un entero positivo" y salir
+    # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
     if m <=0:
         print("Error: La altura debe ser un entero positivo")
         return
-    
-    # Parte 1: triangulo creciente
-    for i in range(1, m + 1):
-        print(s * i)
 
-    # parte 2: Triangulo decreciente
-    for i in range(m -1, 0, -1):
-        print(s * i)
+    #Parte 1: Triángulo Creciente
+    for i in range(1, m + 1):
+        print(s*i)
+    
+    #Parte 2: Triángulo Decreciente
+    for i in range(m - 1, 0, -1):
+        print(s * i)
