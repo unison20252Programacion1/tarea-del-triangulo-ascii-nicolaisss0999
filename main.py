ea37cb7c2e619125fa379a9af2066a2dc82a4233
nicolaisss0999 <nd22070507nicoledurazo@gmail.com>
2025-11-21 14:53:42 -0700

    Correct main function check and call triangulo_simetrico
    Fix typo in main function check and call triangulo_simetrico.

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ def main():
         return
 
     # TODO: llamar a la función triangulo_simetrico con los parámetros m y s
+    triangulo_simetrico(m, s)
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
