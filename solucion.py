6f048a5c8ccaffad2d3bff22d130504123bb95ec
nicolaisss0999 <nd22070507nicoledurazo@gmail.com>
2025-11-21 14:29:15 -0700

    Fix error message for invalid height input

--- solucion.py
+++ solucion.py
@@ -3,7 +3,7 @@
 def triangulo_simetrico(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: la altura debe ser un entero positivo" y salir
     if m <=0:
-        print("error: la altura debe ser un entero positivo")
+        print("Error: La altura debe ser un entero positivo")
         return
     
     # Parte 1: triangulo creciente
